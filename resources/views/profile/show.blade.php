4291d55a7dfb94346475306651e1c8ffcd596f4b
Ben-Fodda <ilias.benfodda@student.odisee.be>
2026-05-18 18:29:17 +0200

    Openbare profiel pagina

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,3 +1,25 @@
 <x-app-layout>
-    <h1>{{ $user->name }}</h1>
+    <div>
+
+        <h1>{{ $user->name }}</h1>
+
+        @if($user->profile_picture)
+            <img
+                src="{{ asset('storage/' . $user->profile_picture) }}"
+                alt="Profile picture"
+                class="w-24 h-24 rounded-full object-cover"
+            >
+        @endif
+
+        @if($user->birthday)
+            <p>Geboortedatum: {{ $user->birthday }}</p>
+        @endif
+
+        @if($user->about_me)
+            <p>Over mij: {{ $user->about_me }}</p>
+        @endif
+
+        <p>Lid sinds: {{ $user->created_at->format('d/m/Y') }}</p>
+
+    </div>
 </x-app-layout>
